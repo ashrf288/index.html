29699e05e594fbcbcbcec7409416e8aefa94124c
ashrfMathkour <>
2021-05-27 13:45:22 +0000

    update

---
+++ logic.js
@@ -0,0 +1,29 @@
+// this is my first jaca script file 
+
+
+var name =  prompt("what is you name ? ");
+
+alert("hello " + name);
+  //  name =  prompt("please add you name  ? ");
+
+
+
+var age = prompt("and you age " + name+ "  ? ");
+if (age>18){
+prompt ("so your name is  " +  name + " and you are  "+ age  +"  years old! " );
+
+alert ("nice !")
+} else{
+  alert("try another link ");
+}
+
+
+ document.write("<p> you are viewing this page as <strong> " + 
+name  + "</strong> </p>" );
+
+ 
+
+
+
+
+
