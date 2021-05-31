c24d545fa900b21f7c4edec46158d4153aade372
ashrfMathkour <>
2021-05-31 13:16:13 +0000

    css

--- logic.js
+++ logic.js
@@ -4,22 +4,43 @@
 var name =  prompt("what is you name ? ");
 
 alert("hello " + name);
-  //  name =  prompt("please add you name  ? ");
+
+var pizzaSlice =  prompt("how many slices of pizza you want from (1-6) ? ");
 
 
 
-var age = prompt("and you age " + name+ "  ? ");
-if (age>18){
-alert ("so your name is  " +  name + " and you are  "+ age  +"  years old! " );
 
-alert ("nice !")
-} else{
-  alert("try another link i think you are  a child  ");
+order();
+
+function order (){
+  // if user inputed a num more than 6
+while(pizzaSlice > 6){
+  alert ('plz enter a reasonable number');
+  pizzaSlice =  prompt("how many slices of pizza you want ? ");
+  
+} 
+// if user inputed a num less than zero
+while(pizzaSlice <= 0){
+  alert ('plz enter a reasonable number');
+  pizzaSlice =  prompt("how many slices of pizza you want ? ");
+  
+}
+// if user inputed a num between 1-6 
+while (pizzaSlice <= 6){
+for (var i =0; pizzaSlice >i;i++){
+  if(pizzaSlice==0){
+  
+    break;
+  }
+
+   document.write("<h2>" + 'slice number' + ' ' + (i+1)+' here you go'+"</h2>");
+    document.write('<img src="https://img.huffingtonpost.com/asset/57c98304170000192ac7736c.jpg"/>');
+}
+ break;
+
 }
 
-
- document.write("<p> you are viewing this page as <strong> " + 
-name  + "</strong> </p>" );
+}
 
  
 
