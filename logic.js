3cb3956747db88af607396521e168dfaf5a2a817
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-05-31 16:24:00 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -9,35 +9,32 @@ var pizzaSlice =  prompt("how many slices of pizza you want from (1-6) ? ");
 
 
 
-<<<<<<< HEAD
+
 
 order();
 
 function order (){
   // if user inputed a num more than 6
-=======
+
 var pizzaSlice =  prompt("how many slices of pizza you want from (1-6) ? ");
->>>>>>> 9d2be8736a7860e82f352d16c8a4c473547de38b
 while(pizzaSlice > 6){
   alert ('plz enter a reasonable number');
   pizzaSlice =  prompt("how many slices of pizza you want ? ");
   
-<<<<<<< HEAD
+
 } 
 // if user inputed a num less than zero
-=======
+
 }
->>>>>>> 9d2be8736a7860e82f352d16c8a4c473547de38b
 while(pizzaSlice <= 0){
   alert ('plz enter a reasonable number');
   pizzaSlice =  prompt("how many slices of pizza you want ? ");
   
 }
-<<<<<<< HEAD
+
 // if user inputed a num between 1-6 
 while (pizzaSlice <= 6){
 for (var i =0; pizzaSlice >i;i++){
-=======
 
   
 
@@ -46,16 +43,16 @@ while (pizzaSlice <= 6){
 
 for (var i =0; pizzaSlice >i;i++){
 
->>>>>>> 9d2be8736a7860e82f352d16c8a4c473547de38b
+
   if(pizzaSlice==0){
   
     break;
   }
-<<<<<<< HEAD
+
 
    document.write("<h2>" + 'slice number' + ' ' + (i+1)+' here you go'+"</h2>");
     document.write('<img src="https://img.huffingtonpost.com/asset/57c98304170000192ac7736c.jpg"/>');
-=======
+
   
 
    document.write("<h2>" + 'slice number' + ' ' + (i+1)+' here you go'+"</h2>");
@@ -63,16 +60,16 @@ for (var i =0; pizzaSlice >i;i++){
 }
  break;
 
->>>>>>> 9d2be8736a7860e82f352d16c8a4c473547de38b
+
 }
  break;
 
 }
 
-<<<<<<< HEAD
+
 }
-=======
->>>>>>> 9d2be8736a7860e82f352d16c8a4c473547de38b
+
+
 
  
 
