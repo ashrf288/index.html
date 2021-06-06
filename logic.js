ba748cf475087a2d8c218512745b8da123469179
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-06-06 16:03:38 +0300

    js and css added

--- logic.js
+++ logic.js
@@ -2,17 +2,28 @@
 
 
 let name =  prompt("what is you name ? ");
+alert(`cool name ${name}  `)
 console.log(name);
 
-alert("hello " + name);
 
-let pizzaSlice =  prompt("how many slices of pizza you want from (1-6) ? ");
+let age= prompt (`hi ${name}, so how old are you ?`);
+alert(`${age} years  ,  nice  ${name}  your old enough :)`)
+  console.log(age);
 
-console.log (pizzaSlice);
-
-
+let pizzaSlice =  prompt(`how many slices of pizza you want ${name} ? from (1-6) `);
 
 order();
+alert(`${pizzaSlice} slices, wow seems like your very hungry !`);
+console.log (pizzaSlice);
+
+let notes= prompt(`${name} do you have any notes  with the order 
+(if not simply type none) ?`);
+console.log(notes);
+
+alert(`so your name is ${name} , and your ${age} years old
+and you want ${pizzaSlice} slices,  and your notes are : ${notes}?`);
+
+
 
 function order (){
   // if user inputed a num more than 6
@@ -36,7 +47,7 @@ while(pizzaSlice <= 0){
 // if user inputed a num between 1-6 
 
 while (pizzaSlice <= 6){
-
+  
 for (let i =0; pizzaSlice >i;i++){
 
    document.write("<h2>" + 'slice number' + ' ' + (i+1)+' here you go'+"</h2>");
@@ -44,18 +55,11 @@ for (let i =0; pizzaSlice >i;i++){
 
    
 }
+
+document.write(` <h3>hope  you enjoy your meal  ${name} </h3>`);
  break;
 
 
 }
-
-
-
-
-
- 
-
-
-
 
 
